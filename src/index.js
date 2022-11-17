22f24844c19b0b6a9a7a5f7dc7a7e146cae56db9
Becky449 <rabnab12@gmail.com>
2022-11-17 19:08:04 +0300

    update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import ToDoContainer from "./Components/ToDoContainer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ToDoContainer />
+    <ToDoContainer/>
   </React.StrictMode>
 );
